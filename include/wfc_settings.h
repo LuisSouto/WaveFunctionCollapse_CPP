4961c0da870f9b8905976ff2bc064d85f54a4f58
LuisSouto <luisouto.94@gmail.com>
2026-07-19 21:22:54 +0200

    Remove FIXED from boundary enum

--- include/wfc_settings.h
+++ include/wfc_settings.h
@@ -2,16 +2,16 @@
 
 #include <cstdint>
 
-enum BoundaryCondition { NONE, PERIODIC_X, PERIODIC_Y, PERIODIC, FIXED };
+enum BoundaryCondition { NONE, PERIODIC_X, PERIODIC_Y, PERIODIC };
 
 enum CellSelectionStrategy { SCANLINE, ENTROPY };
 
 struct WFCSettings {
-  uint8_t dimensions = 2;
-  uint8_t boundary_condition = BoundaryCondition::PERIODIC_X;
-  uint8_t cell_selection_strategy = CellSelectionStrategy::SCANLINE;
-  uint32_t block_size_x = 16;
-  uint32_t block_size_y = 16;
-  uint8_t enable_backtracking = false;
-  uint8_t force_boundary_patterns = true;
+	uint8_t dimensions = 2;
+	uint8_t boundary_condition = BoundaryCondition::PERIODIC_X;
+	uint8_t cell_selection_strategy = CellSelectionStrategy::SCANLINE;
+	uint32_t block_size_x = 16;
+	uint32_t block_size_y = 16;
+	uint8_t enable_backtracking = false;
+	uint8_t force_boundary_patterns = true;
 };
