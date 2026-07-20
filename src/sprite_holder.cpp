6b9a96a2cd37ea5592321341011c4855d72b482b
LuisSouto <luisouto.94@gmail.com>
2026-07-20 15:54:58 +0200

    Use assert instead of throw.

--- src/sprite_holder.cpp
+++ src/sprite_holder.cpp
@@ -1,13 +1,14 @@
-#include <cstddef>
 #include <sprite_holder.h>
 
 /* Create a unique hash for each pixel in the image.*/
 // WARNING: this assumes RBG format, it will not work otherwise
 void SpriteHolder::computePixelHashes() {
-  pixel_hashes.resize(num_pixels);
-  for (size_t i = 0; i < num_pixels; i++) {
-    pixel_hashes[i] = image_pixels[i * channels] |
-                      (image_pixels[i * channels + 1] << 8) |
-                      (image_pixels[i * channels + 2] << 16);
-  }
+	pixel_hashes.resize(num_pixels);
+	for (size_t i = 0; i < num_pixels; i++) {
+		pixel_hash_t pixel_hash = 0;
+		for (size_t c = 0; c < channels; c++) {
+			pixel_hash += image_pixels[i * channels + c] << (c * 8);
+		}
+		pixel_hashes[i] = pixel_hash;
+	}
 }
--- src/sprite_reader.cpp
+++ src/sprite_reader.cpp
@@ -1,28 +1,23 @@
-#include <cstddef>
-#include <cstdint>
 #include <sprite_reader.h>
 #include <stb_image.h>
+#include <cstddef>
+#include <cstdint>
 #include <vector>
 
-SpriteHolder SpriteReader::loadFromPng(const char *filename,
-                                       int desired_channels) {
-  int width;
-  int height;
-  int n_channels;
+SpriteHolder SpriteReader::loadFromPng(const char *filename, int desired_channels) {
+	int width;
+	int height;
+	int n_channels;
 
-  unsigned char *image_data =
-      stbi_load(filename, &width, &height, &n_channels, desired_channels);
+	unsigned char *image_data = stbi_load(filename, &width, &height, &n_channels, desired_channels);
 
-  if (!image_data) {
-    throw std::runtime_error("Failed to load image: " + std::string(filename));
-  }
+	assert(image_data && "Failed to load image");
 
-  // Make into flat array for cache locality
-  size_t total_bytes =
-      static_cast<size_t>(width) * height * desired_channels * sizeof(uint8_t);
-  std::vector<uint8_t> image_pixels_rgb(image_data, image_data + total_bytes);
-  stbi_image_free(image_data);
+	// Make into flat array for cache locality
+	size_t total_bytes = static_cast<size_t>(width) * height * desired_channels * sizeof(uint8_t);
+	std::vector<uint8_t> image_pixels_rgb(image_data, image_data + total_bytes);
+	stbi_image_free(image_data);
 
-  return SpriteHolder(static_cast<size_t>(width), static_cast<size_t>(height),
-                      desired_channels, image_pixels_rgb);
+	return SpriteHolder(static_cast<size_t>(width), static_cast<size_t>(height), desired_channels,
+			image_pixels_rgb);
 }
