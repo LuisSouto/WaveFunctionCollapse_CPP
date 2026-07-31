8b3ecdb1ca1839c9e62873d065f34939ee23aec6
LuisSouto <luisouto.94@gmail.com>
2026-08-01 00:02:13 +0200

    Add horizontal and vertical flips

--- include/sprite_transforms.h
+++ include/sprite_transforms.h
@@ -8,7 +8,9 @@ struct SpriteTransforms {
 		ROTATE_90 = 2,
 		ROTATE_180 = 4,
 		ROTATE_270 = 8,
-		ALL_TRANSFORMS = 15
+		FLIP_HORIZONTAL = 16,
+		FLIP_VERTICAL = 32,
+		ALL_TRANSFORMS = 63
 	};
 };
 
@@ -28,5 +30,13 @@ inline size_t rotate270(size_t x, size_t y, size_t width, size_t height) {
 	return (width - 1 - y) + x * width;
 }
 
+inline size_t flip_horizontal(size_t x, size_t y, size_t width, size_t height) {
+	return (width - 1 - x) + y * width;
+}
+
+inline size_t flip_vertical(size_t x, size_t y, size_t width, size_t height) {
+	return x + (height - 1 - y) * width;
+}
+
 inline std::vector<TransformFunction> transformFunctions = { identity, rotate90, rotate180,
-	rotate270 };
+	rotate270, flip_horizontal, flip_vertical };
