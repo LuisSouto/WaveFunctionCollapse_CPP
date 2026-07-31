fa6a89f04d31a4fd2124346639f0c853d6f15d90
LuisSouto <luisouto.94@gmail.com>
2026-07-31 05:03:20 +0200

    Revert "Buggy implementation of rotation"
    This reverts commit 4a37060b88ddda74cff09e279e9f181f273617f6.

--- include/directions.h
+++ include/directions.h
@@ -4,9 +4,9 @@
 
 enum Directions { UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3 };
 
-const static std::vector<int> DX = {0, 1, 0, -1};
-const static std::vector<int> DY = {1, 0, -1, 0};
+const static std::vector<int> DX = { 0, 1, 0, -1 };
+const static std::vector<int> DY = { -1, 0, 1, 0 };
 
-static size_t rotateDirectionClockwise(Directions direction, size_t num_rotations) {
-  return (direction + num_rotations) % 4;
+size_t rotateDirectionClockwise(size_t direction, size_t num_rotations) {
+	return (direction + num_rotations) % 4;
 }
